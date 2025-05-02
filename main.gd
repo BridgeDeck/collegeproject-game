a5b90db364652e82ce5480fff6f235b8867c1f24
BridgeDeck <littlefast2@gmail.com>
2025-05-02 17:24:13 +0800

    more updates

--- main.gd
+++ main.gd
@@ -2,9 +2,7 @@
 extends Node
 
 func _ready() -> void:
-	LoadingScreen.loading_text = "Loading your mom, gottem ;D"
+	#LoadingScreen.loading_text = "Loading your mom, gottem ;D"
 	#LoadingScreen.active = true
-	MapHandler.load_map_by_path("res://maps/base_map.tscn")
-
-func _process(delta: float) -> void:
+	#MapHandler.load_map_by_path("res://maps/base_map.tscn")
 	pass
