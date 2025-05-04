da1f08753a22f7d4d886a5d861ca71b2a54650fd
BridgeDeck <littlefast2@gmail.com>
2025-05-04 16:40:43 +0800

    updated main menu autoload and ui

--- ui/main_menu/autoload.gd
+++ ui/main_menu/autoload.gd
@@ -5,9 +5,18 @@ var node_main_menu:Control
 func _enter_tree() -> void:
 	node_main_menu = preload("res://ui/main_menu/main_menu.tscn").instantiate()
 	add_child(node_main_menu)
-	
+	node_main_menu.visible = false
+
+	# get_tree().root.ready.connect(_on_root_ready, CONNECT_ONE_SHOT)
+
+
 func _process(delta: float) -> void:
-	# Check if any other important process is running,  otherwise, display main menu.
-	if MapHandler.current_map != null:
-		return
+	var should_be_visible = true
 	
+	if Engine.is_editor_hint():
+		should_be_visible = false
+
+	if MapHandler.current_map != null:
+		should_be_visible = false
+	
+	node_main_menu.visible = should_be_visible
--- ui/main_menu/main_menu.gd
+++ ui/main_menu/main_menu.gd
@@ -1,5 +1,7 @@
 extends Control
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _ready() -> void:
+	$Buttons/Play.pressed.connect(_on_playbutton_pressed)
+
+func _on_playbutton_pressed():
+	MapHandler.load_map_by_path("res://maps/base_map.tscn")
