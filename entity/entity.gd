ff07211f9d9a9e840606baddf333c06bc2515f56
BridgeDeck <littlefast2@gmail.com>
2025-05-06 17:26:38 +0800

    tried to do walljumping, commented out for now

--- entity/entity.gd
+++ entity/entity.gd
@@ -11,12 +11,18 @@ const FLAG_ACTION3 = 1<<4
 @export var jump_force:float = 8
 @export var acceleration:float = 18*3
 @export var air_speed_modifier:float = 1.0
+@export var max_jump_count:int = 1
 
+var jump_count:int = max_jump_count
 var action_previous:int
 var action:int
 var equipped:int
 
 var force:Vector3
+
+# var previous_global_position:Vector3
+# var previous_velocity:Vector3
+# var walljump_impulse:Vector3
 
 func add_force(applyforce:Vector3, vertical_only:bool):
 	if vertical_only:
@@ -32,11 +38,17 @@ func player_input(
 ):
 	action = actionflags
 	add_force((camera_basis * Vector3(movement_dir.x, 0, movement_dir.y)).slide(up_direction).normalized(),false)
+	equipped = equip
 
+func _ready() -> void:
+	if not Engine.is_editor_hint():
+		$CrouchCollision/CSGBox3D.queue_free()
+		$CrouchCollision/CSGBox3D.queue_free()
 
 func _physics_process(delta: float) -> void:
 	if Engine.is_editor_hint():
 		return
+
 
 	#Crouching
 	if (action & FLAG_CROUCH) == FLAG_CROUCH:
@@ -53,26 +65,56 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta 
 	else:
 		velocity = velocity - (velocity*up_direction)
-	
-	# jumping
-	# Check if previous action flags did not contain FLAG_JUMP and then check if current action flags do
-	if (not ((action_previous & FLAG_JUMP) == FLAG_JUMP)) and ((action & FLAG_JUMP) == FLAG_JUMP):
-		velocity = (velocity - (velocity*up_direction)) + up_direction * jump_force
+		jump_count = max_jump_count
 	
 	var modifiers = 1
 	if not is_on_floor():
 		modifiers -= air_speed_modifier
 	
-	# if not is_on_floor():
-	# 	# infinite momentum
-	# 	velocity = (velocity - velocity*up_direction) + (force.normalized() * acceleration*delta*modifiers) + velocity*up_direction
-	# else: 
-	# 	velocity = (velocity - velocity*up_direction).move_toward(force, acceleration*delta*modifiers) + velocity*up_direction
+	# Cannot add momentum midair, but you can redirect it
+	# if is_on_floor():
 	velocity = (velocity - velocity*up_direction).move_toward(force, acceleration*delta*modifiers) + velocity*up_direction
 
+	# a pathetic implementation of walljumping, do we even want wall jumping? or are midair jumps fine.
+	# if (not is_on_floor()) and is_on_wall() and walljump_impulse.is_zero_approx():
+	# 	var query = PhysicsRayQueryParameters3D.new()
+	# 	query.from = previous_global_position
+	# 	query.to = previous_global_position + ((previous_velocity-previous_velocity*up_direction).normalized() * speed)
+	# 	query.collision_mask = 1<<0
+	# 	query.exclude.append(self.get_rid())
+	# 	var space = PhysicsServer3D.space_get_direct_state(get_world_3d().space)
+	# 	var result = space.intersect_ray(query)
+	# 	var result_normal:Vector3 = result.get("normal", Vector3())
+
+	# 	if result_normal.max_axis_index() != Vector3.AXIS_Y:
+	# 		walljump_impulse = result_normal * speed
+	# else:
+	# 	walljump_impulse = Vector3()
+
+	# jumping
+	# Check if previous action flags did not contain FLAG_JUMP
+	# THEN check if current action flags do
+	# THEN check if you can still use jumps
+	# THEN check if you are even allowed to jump
+	if (not ((action_previous & FLAG_JUMP) == FLAG_JUMP)) \
+		and ((action & FLAG_JUMP) == FLAG_JUMP) \
+		and jump_count > 0 \
+		and can_jump():
+		jump_count -= 1
+		velocity = (velocity - (velocity*up_direction)) + up_direction * jump_force
+
+		# if not walljump_impulse.is_zero_approx():
+			# velocity = walljump_impulse + velocity*up_direction
+			# walljump_impulse = Vector3()
+	
+	# previous_velocity = velocity
 	move_and_slide()
 
 	action_previous = action
+	# previous_global_position = global_position
+
+func can_jump()->bool:
+	return is_on_floor() or (!is_on_floor() and is_on_wall())
 
 ## Generate a bitflag int representing the different actions an entity can do
 static func generate_action_flag(
